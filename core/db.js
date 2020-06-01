c2a983c094ce94d5bbcd6a0c12ea2c8047996707
Ashish Yoel <ashishyoel23@gmail.com>
2020-06-01 13:56:45 +0530

    packages added

--- core/db.js
+++ core/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const db = 'mongodb://localhost:27017/boilerplate'
+const db = 'mongodb://localhost:27017/{name-of-the-collection}'
 
 const connectDb = async () => {
     try {
